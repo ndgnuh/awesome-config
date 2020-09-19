5cabee9df06ff432ae7aff5746967bfe1c349658
hung <ndgnuh@protonmail.com>
2020-09-19 16:15:59 +0700

    ibus rewrite

--- IBus.lua
+++ IBus.lua
@@ -1,69 +1,137 @@
-local sh = re"sh"
-local ic = re"icon"
-local awful = re"awful"
-local widget = re"wibox.widget"
-local gobject = re"gears.object"
-local gtable = re"gears.table"
-local lgi = re"lgi"
-
+--[[ module doc
+this module should provide these functionality:
+- cycle e1, e2, ...
+- get
+- watch callback
+- key mod key
+]]
+local sh = require"sh"
+local ic = require"icon"
+local awful = require"awful"
+local gobject = require"gears.object"
+local lgi = require"lgi"
 local IBus = lgi.IBus
 
 local script = "ibus-cycle.sh"
 
-local module = {
-	bus = IBus.Bus()
-}
+local module =
+  gobject {
+    class = { bus = IBus.Bus() }
+  }
 
--- @iconmap
--- engine name = icon file
-module.iconmap = {
-	["Bamboo"] = "mdi-ibus-vietnamese.svg",
-	["xkb:us::eng"] = "ibus-english.svg",
-	["default"] = "ibus-english.svg"
-}
+local list_engine_desc =
+  {
+    "author",
+    "description",
+    "hotkeys",
+    "icon",
+    "icon",
+    "language",
+    "layout",
+    "layout",
+    "layout",
+    "license",
+    "longname",
+    "name",
+    "rank",
+    "setup",
+    "symbol",
+    "textdomain",
+    "version"
+  }
 
-module.widget = widget {
-	widget = widget.imagebox,
-	image = ic(module.iconmap['default']),
-	buttons = awful.button({}, 1, partial(sh, script))
-}
+local describe =
+  function (engine_desc)
+    local result = {}
+    for _, key in pairs(list_engine_desc) do
+      result[key] = engine_desc[key]
+    end
+    return result
+  end
 
+-- @get
+-- returns current the global engine description
+module.get =
+  function (self)
+    local engine_desc =
+      self.engine or
+      self.bus:get_global_engine()
+    if not self.engine then
+      self.engine = engine_desc
+    end
+    return
+      describe(engine_desc)
+  end
+
+-- @set
+-- set global engine
+module.set =
+  function(self, name)
+    self.bus:set_global_engine(name)
+  end
+
+-- @setup
+-- set the list of engine to cycle
+module.setup =
+  function (self, engines)
+    self.engines = engines
+    return engines
+  end
 
 -- @cycle
 -- cycle between ibus engine
 -- the engines is defined in the script
-module.cycle = partial(sh, script)
+-- module.cycle = partial(sh, script)
+module.cycle =
+  function (self, engines)
+    engines =
+      engines or
+      self.engines
+    local idx = 1
+    local current_name =
+      self.bus:get_global_engine().name
+    for i, enginename in ipairs(engines) do
+      if enginename == current_name then
+        idx = i + 1
+        break
+      end
+    end
+    if idx > #engines then
+      idx = 1
+    end
+    local newengine =
+      engines[idx]
+    self.bus:set_global_engine(newengine)
+  end
 
 -- @key
 -- the switch key
 -- for binding
-module.key = function(mod, key)
-	awful.key(mod, key, module.cycle)
-end
+module.key =
+  function (mod, key, engines)
+    return
+      awful.key(mod, key, partial2(module.cycle, module, engines))
+  end
 
--- @fillwidget
--- TBA
-module.fillwidget = function(containerGetter)
-	awful.screen.connect_for_each_screen(function(s)
-		s:connect_signal("", function(s)
-			containerGetter(s):setup(module.widget)
-		end)
-	end)
-end
+-- @watch
+-- signal connecter
+module.watch =
+  function(self, callback)
+    module.bus.on_global_engine_changed =
+      function(bus, name)
+        local engine =
+          bus:get_engines_by_names({name})[1]
+        callback(describe(engine))
+      end
+  end
 
--- @signal
--- update the wibox after
--- switching engine is done
--- or dump the error
-sh:connect_signal(script, function(self, engine, err)
-	-- ibus engine command annoyingly return the
-	engine = engine:match("[a-zA-Z0-9:;]+")
-	if err and err ~= "" then
-		dump(err)
-	else
-		local img = module.iconmap[engine]
-		module.widget:set_image(ic(img))
-	end
-end)
+-- reset cache after change engine [[
+module.watch(
+  function()
+    module.engine = nil
+  end
+)
+--]]
 
+module.bus:set_watch_ibus_signal(true)
 return module
