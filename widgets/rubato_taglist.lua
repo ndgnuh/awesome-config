58ada68bdba228b01ec46fb795986af2cdfb040e
Hung Nguyen <hungnd@hiworld.com.vn>
2022-10-24 09:45:45 +0700

    taglist buttons

--- widgets/rubato_taglist.lua
+++ widgets/rubato_taglist.lua
@@ -21,6 +21,30 @@ local animation = gears.cache(function(ref)
 	return timed
 end)
 
+-- buttons
+local taglist_buttons = gears.table.join(
+awful.button({}, 1, function(t)
+	t:view_only()
+end),
+awful.button({ modkey }, 1, function(t)
+	if client.focus then
+		client.focus:move_to_tag(t)
+	end
+end),
+awful.button({}, 3, awful.tag.viewtoggle),
+awful.button({ modkey }, 3, function(t)
+	if client.focus then
+		client.focus:toggle_tag(t)
+	end
+end),
+awful.button({}, 4, function(t)
+	awful.tag.viewnext(t.screen)
+end),
+awful.button({}, 5, function(t)
+	awful.tag.viewprev(t.screen)
+end)
+)
+
 -- prevent the animation getting cleaning by GC
 animation._cache = setmetatable(animation._cache, nil)
 
