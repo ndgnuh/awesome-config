2f3c4d39f7b75f0147f0da15dd4aa482026e59ad
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-12-18 00:21:21 +0700

    because

---
+++ _copypaste.lua
@@ -0,0 +1,13 @@
+local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
+local widget = require("wibox.widget")
+local container = require("wibox.container")
+local layout = require("wibox.layout")
+local shape = require("gears.shape")
+local color = require("gears.color")
+local capi = {
+    awesome = awesome,
+    root = root,
+    client = client
+}
