76332a4e294379ff6bdb74a5280cc586e7ca8aad
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-07 13:31:45 +0700

    add stuffs from the bg widget

--- widgets/pill_container.lua
+++ widgets/pill_container.lua
@@ -21,7 +21,8 @@ local function wrapper()
 				left = 10,
 				right = 10,
 				{
-					widget = wibox.layout.stack,
+					widget = wibox.layout.fixed.horizontal,
+					spacing = 10,
 					id = 'content'
 				}
 			}
@@ -33,21 +34,32 @@ function pill:child(id)
 	return self.wrapper:get_children_by_id(id)[1]
 end
 
-function pill:set_padding(padding)
-	padding = padding or beautiful.pill_bg or 0
-	self:child('margin').margins = padding
+function pill:set_margins(margins)
+	margins = margins or beautiful.pill_bg or 0
+	self:child('margin').margins = margins
+	self:emit_signal("widget::redraw_needed")
+end
+
+function pill:set_spacing(spacing)
+	self:child('content'):set_spacing(spacing)
+	self:emit_signal("widget::layout_changed")
 end
 
 function pill:set_bg(bg)
 	bg = bg or beautiful.pill_bg
 	self:child('background'):set_bg(bg)
+	self:emit_signal("widget::redraw_needed")
 end
 
 function pill:set_children(children)
 	local content = self:child("content")
+	local spacing = content.spacing
 	content:reset()
-	content:add(children[1])
+	for _, child in ipairs(children) do
+		content:add(child)
+	end
 	self:emit_signal("widget::layout_changed")
+	self:emit_signal("widget::redraw_needed")
 end
 
 function pill:fit(context, width, height)
@@ -58,9 +70,10 @@ function pill:layout(_, width, height)
 	return { base.place_widget_at(self.wrapper, 0, 0, width, height) }
 end
 
-local function new(widget, bg, padding)
+local function new(widget, bg, margins)
 	local ret = base.make_widget(nil, nil,  {enable_properties=true})
 	ret.wrapper = wrapper()
+	gtable.crush(ret, wibox.container.background, true)
 	gtable.crush(ret, pill, true)
 	return ret
 end
