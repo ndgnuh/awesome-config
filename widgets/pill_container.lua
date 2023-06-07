fdd5b9aabf47749c507341e3aa35a25e433d1009
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-07 11:26:07 +0700

    rewrite

--- widgets/pill_container.lua
+++ widgets/pill_container.lua
@@ -8,7 +8,7 @@ local beautiful = require("beautiful")
 
 local pill = {mt = {}}
 
-pill.children = wibox.widget{
+pill.wrapper = wibox.widget{
 	widget = wibox.container.margin,
 	id = 'margin',
 	{
@@ -27,57 +27,34 @@ pill.children = wibox.widget{
 	}
 }
 
-function pill:get_child(id)
-	return self.children:get_children_by_id(id)[1]
+function pill:child(id)
+	return self.wrapper:get_children_by_id(id)[1]
+end
+
+function pill:set_padding(padding)
+	padding = padding or beautiful.pill_bg or 0
+	self:child('margin').margins = padding
 end
 
 function pill:set_bg(bg)
 	bg = bg or beautiful.pill_bg
-	self:get_child('background'):set_bg(bg)
-end
-
-function pill:set_padding(padding)
-	padding = padding or beautiful.pill_padding
-	self:get_child('margin').margins = padding
-	self:emit_signal("widget::redraw_needed")
-	self:emit_signal("widget::layout_changed")
-end
-
-function pill:set_widget(widget)
-    if widget then
-        base.check_widget(widget)
-    end
-    self._private.widget = widget
-    self:emit_signal("widget::layout_changed")
+	self:child('background'):set_bg(bg)
 end
 
 function pill:set_children(children)
-	local content = self.children:get_children_by_id("content")[1]
+	local content = self:child("content")
 	content:reset()
 	content:add(children[1])
 	self:emit_signal("widget::layout_changed")
 end
 
-
--- wrapping function
-function pill:draw(ctx, cr, width, height)
-	self.children:draw(ctx, cr, width, height)
-end
-
 function pill:layout(_, width, height)
-	return { base.place_widget_at(self.children, 0, 0, width, height) }
+	return { base.place_widget_at(self.wrapper, 0, 0, width, height) }
 end
-
-function pill:fit(ctx, width, height)
-	return base.fit_widget(self, ctx, self.children, width, height)
-end
-
 
 local function new(widget, bg, padding)
-	local ret = base.make_widget(nil, nil, {enable_properties = true})
+	local ret = base.make_widget(nil, nil,  {enable_properties=true})
 	gtable.crush(ret, pill, true)
-	ret:set_padding(padding)
-	ret:set_bg(bg)
 	return ret
 end
 
